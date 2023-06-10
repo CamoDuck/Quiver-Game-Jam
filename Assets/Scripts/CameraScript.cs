fe8157c289eeab38daf319908ab757545e2728cc
CamoDuck <cubebox52@gmail.com>
2023-06-09 22:19:30 -0700

    Added basic player movement
    - Added basic WASD movement

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    /// CONSTANT ///
+    [SerializeField] Rigidbody2D body;
+
+    float minMoveSpeed = 3;
+    const float maxDist = 100;
+
+    /// VARYING ///
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    void FixedUpdate() {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    /// CONSTANT ///
+    PlayerInput playerInput;
+    [SerializeField] Rigidbody2D body;
+    const float walkSpeed = 5;
+
+    /// VARYING ///
+
+    void Awake() {
+        playerInput = new PlayerInput();
+        playerInput.Enable();
+    }
+    void Start() {
+
+    }
+
+    void Update() {
+        Vector2 movementVector = playerInput.Default.Movement.ReadValue<Vector2>();
+        body.velocity = movementVector * walkSpeed;
+    }
+}
