3644cb125e1f864824c26652de8a56086fcc079e
Joshua Bounds <jbounder@gmail.com>
2023-06-11 13:30:25 -0700

    Added animations to detail props

--- Assets/Scripts/DetailProp.cs
+++ Assets/Scripts/DetailProp.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class DetailProp : MonoBehaviour
 {
     public Animator animator;
+    public string playerTag;
 
-    void OnTriggerEnter(Collider collider)
+    void OnTriggerEnter2D(Collider2D collider)
     {
-        Debug.Log("Triggered");
+        if (collider.gameObject.tag != playerTag) return;
+
+        animator.SetBool("triggered", true);
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag != playerTag) return;
+
+        animator.SetBool("triggered", false);        
     }
     
 }
