cb5fdf6769b5e140d6543e9061a84a73b291b607
Nikeligo <77532096+Nikeligo@users.noreply.github.com>
2023-06-11 00:38:45 -0700

    transtition
    got a bit of a transition going in the Player_UI Script. going to bed though

--- Assets/Scripts/PlayerUI.cs
+++ Assets/Scripts/PlayerUI.cs
@@ -12,6 +12,14 @@ public class PlayerUI : MonoBehaviour
     [SerializeField] TextMeshProUGUI choice3Text;
     [SerializeField] TextMeshProUGUI healthText;
     [SerializeField] TextMeshProUGUI enemyHealthText;
+
+    [SerializeField] GameObject dialogueBox;
+    [SerializeField] GameObject choice1Box;
+    [SerializeField] GameObject choice2Box;
+    [SerializeField] GameObject choice3Box;
+    [SerializeField] GameObject healthBox;
+    [SerializeField] GameObject enemyHealthBox;
+
     [SerializeField] GameObject throwableCoworker;
     [SerializeField] Rigidbody2D body;
     [SerializeField] new BoxCollider2D collider;
@@ -45,7 +53,7 @@ public class PlayerUI : MonoBehaviour
         updateHealthUI();
         if (currentHealth < 0) {
             Death();
-        } 
+        }
     }
 
     void Death() {
@@ -60,15 +68,15 @@ public class PlayerUI : MonoBehaviour
         }
     }
 
-    void OnTriggerEnter2D (Collider2D other) {
+    void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Coworker") {
             coworker = other.GetComponent<BaseCoworker>();
-            StartInteraction();
+            StartCoroutine(InteractionTransition(1));
         }
     }
 
     void EndInteraction() {
-        coworker.followTarget = followers.Count==0? body: followers[followers.Count-1].body;
+        coworker.followTarget = followers.Count == 0 ? body : followers[followers.Count - 1].body;
         followers.Add(coworker);
         coworker = null;
         UI.transform.gameObject.SetActive(false);
@@ -76,7 +84,7 @@ public class PlayerUI : MonoBehaviour
     }
 
     IEnumerator ThrowCoworkers() {
-        Vector2 diretion = new Vector2(1,1).normalized;
+        Vector2 diretion = new Vector2(1, 1).normalized;
         float minForceStregth = 5;
         float maxForceStrength = 10;
         float maxThrowDisplacement = 5;
@@ -103,14 +111,14 @@ public class PlayerUI : MonoBehaviour
             clone.GetComponent<SpriteRenderer>().sprite = followerSprite;
             float damage = currentFollower.attackDamage;
             AttackCoworker(damage);
-            if (coworker == null) {yield break;}
+            if (coworker == null) { yield break; }
 
             float displaceX = Random.Range(0, maxThrowDisplacement);
             float displaceY = Random.Range(0, maxThrowDisplacement);
             Vector2 throwDisplacement = new Vector2(displaceX, displaceY);
             Vector2 throwPosition = (Vector2)transform.position - throwDisplacement;
 
-            Vector2 force = diretion * Random.Range(minForceStregth,maxForceStrength);
+            Vector2 force = diretion * Random.Range(minForceStregth, maxForceStrength);
             body.AddForceAtPosition(force, throwPosition, ForceMode2D.Impulse);
 
             float waitTime = Random.Range(0, maxWaitBetweenThrows);
@@ -135,12 +143,36 @@ public class PlayerUI : MonoBehaviour
         choice2Text.text = currentDialog[1].text;
         choice3Text.text = currentDialog[2].text;
     }
-    void StartInteraction() {
+
+    IEnumerator InteractionTransition(int reverse)
+    {
         collider.enabled = false;
         updateHealthUI();
         updateEnemyHealthUI();
-        updateChoices();
         UI.gameObject.SetActive(true);
+        //int i = 0;
+
+        //Vector3 desiredPosition = new Vector3(dialogueBox.transform.position.x, dialogueBox.transform.position.y + (70 * reverse), 0.0f);
+        Vector3 desiredPosition = dialogueBox.transform.position;
+        dialogueBox.transform.position = new Vector3(dialogueBox.transform.position.x, dialogueBox.transform.position.y - (dialogueBox.transform.position.y * 2), 0.0f);
+        while (dialogueBox.transform.position.y < (desiredPosition.y - 5))
+        {
+            Vector3 currentPosition = dialogueBox.transform.position;
+            Vector3 interpolatedPosition = Vector3.Lerp(currentPosition, desiredPosition, Time.deltaTime * 5.0f);
+            dialogueBox.transform.position = interpolatedPosition;
+
+            //dialogueBox.transform.position = new Vector3(dialogueBox.transform.position.x, dialogueBox.transform.position.y + (10 * Time.deltaTime), 0.0f);
+            yield return new WaitForEndOfFrame();
+        }
+        dialogueBox.transform.position = desiredPosition;
+        StartInteraction();
+    }
+
+
+    
+
+    void StartInteraction() {            
+        updateChoices();
     }
 
 
